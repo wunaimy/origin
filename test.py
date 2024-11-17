b1cca6a4f8c6a3021d0f8013136d61bbdd2bebaa
wunaimy <135185729+wunaimy@users.noreply.github.com>
2024-11-17 16:12:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('Hello World!')
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+    return []
